454e3c1c44579101f4cdac3915a0ca0d5d9be3eb
AKASH <akashkr0208@gmail.com>
2024-08-17 20:47:46 +0530

    2nd Commit

--- FirstApp/src/demo/app/Demo.java
+++ FirstApp/src/demo/app/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		
+		StringBuilder sd=new StringBuilder("Hello");
+		System.out.println(sd);
 	}
 
 }
