fb40d272dfd016141875f49f3af615ce7e8db414
AKASH <akashkr0208@gmail.com>
2024-08-17 13:26:53 +0530

    First Commit

---
+++ FirstApp/src/demo/app/First.java
@@ -0,0 +1,10 @@
+package demo.app;
+import java.util.*;
+public class First {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
